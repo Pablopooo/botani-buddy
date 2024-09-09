e21fdee8b4ee74b98ccf31dfef99989fd11ab2ee
Pablopooo <pab.riquelmea@duocuc.cl>
2024-09-09 00:05:25 -0300

    Categoria Plantas final

--- src/app/plantas/plantas.page.ts
+++ src/app/plantas/plantas.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-plantas',
   templateUrl: './plantas.page.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantasPage implements OnInit {
 
-  constructor() { }
+  plantas = [
+    {
+      nombre: 'Mauricio',
+      descripcion: 'Mi linda plantita llamada Mauricio',
+      imagen: 'assets/icon/mauricio.webp'
+    },
+    {
+      nombre: 'Aloe Verita',
+      descripcion: 'Mi plantita de Aloe Vera pa las quemaduras',
+      imagen: 'assets/icon/aloe.webp'
+    },
+    {
+      nombre: 'Tulipancito',
+      descripcion: 'Nunca mires a un tulipan a los ojos',
+      imagen: 'assets/icon/tuli.jpg'
+    }
+  ];
 
+  constructor(private router: Router) { }
   ngOnInit() {
   }
 
+  goToAnadir(){
+    this.router.navigate(['/anadir'])
+  }
+  
 }
