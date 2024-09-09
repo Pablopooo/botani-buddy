d6d928eedac707ee951c2ec21915d9bf6f49bb0a
Pablopooo <pab.riquelmea@duocuc.cl>
2024-09-09 00:25:01 -0300

    Seccion de ayuda

--- src/app/ayudas/ayudas.page.ts
+++ src/app/ayudas/ayudas.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-ayudas',
   templateUrl: './ayudas.page.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AyudasPage implements OnInit {
 
-  constructor() { }
+  plantas = [
+    {
+      nombre: '8 Tips para tu Aloe Vera',
+      descripcion: 'Buenos consejos para mantener tu plantita sana y bonita',
+      imagen: 'assets/icon/alooo.jpg'
+    },
+    {
+      nombre: '¿Cual deberia ser mi primera planta?',
+      descripcion: 'A todos nos gustaria poseer una planta, pero a veces no sabemos por cual empezar, y si ya tienes una cual deberia seguir.',
+      imagen: 'assets/icon/pensando.avif'
+    },
+    {
+      nombre: '¿Que planta de PVZ eres según tu jardín de hogar?',
+      descripcion: '¿Algúna vez te haz preguntado que planta de pvz serías según tu coleccion de plantas hogareñas?, yo tampoco',
+      imagen: 'assets/icon/pvz.avif'
+    }
+  ];
 
+  constructor(private router: Router) { }
   ngOnInit() {
   }
 
+  goToAnadir(){
+    this.router.navigate(['/home'])
+  }
 }
