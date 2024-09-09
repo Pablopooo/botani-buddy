5d84716cd5ffde6b2a45b19d5dab45896dfe0e07
Pablopooo <pab.riquelmea@duocuc.cl>
2024-09-08 21:18:20 -0300

    Eliminacion de camara

--- src/app/anadir/anadir.page.ts
+++ src/app/anadir/anadir.page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-anadir',
   templateUrl: './anadir.page.html',
   styleUrls: ['./anadir.page.scss'],
 })
 export class AnadirPage implements OnInit {
+
 
   constructor(private router: Router) { }
 
